10f1c9e66f01258e7e44c094ffbff98952280168
Paul Adolphson <paul.adolphson@gmail.com>
2022-02-22 02:49:15 -0500

    trainer.py updates

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,6 +8,9 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
 class Trainer():
     def __init__(self, X, y):
@@ -48,6 +51,31 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[JP] [Canada] [padofreo] TaxiFareModel + V2"
 
 
 if __name__ == "__main__":
